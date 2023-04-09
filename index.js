57a6fb2004b5da7636284a35966dea8e937ce7a3
Daniil Burko <volatilepen690@gmail.com>
2023-04-09 16:09:56 +0200

    add function to numbered buttons

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function updateScreen(){
+function updateScreen(value){
     document.getElementById("screen").innerHTML = value;
 }
+
+let number1 = null;
+let operator = "";
+let number2 = null;
+
+
+function display(num){
+    number1 = num;
+    const result = number1 + operator + number2;
+    updateScreen(result);
+}
+
